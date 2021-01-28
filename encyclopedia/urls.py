209f4026f04fc2eb200a8a18d5a20d57a205670c
unknown <wrussell.horton@gmail.com>
2021-01-28 18:30:11 -0500

    January 28 work

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("hello", views.hello, name="hello"),
+    path("error", views.error, name="error"),
+    path("<str:title>", views.title, name="title")
 ]
